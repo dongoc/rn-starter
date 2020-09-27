634bce2922dfceee0bb8e03a5528d3f87e5e3cb0
dongoc <dongoc21hj@gmail.com>
2020-09-27 13:35:26 +0900

    3-5: solving the key issue-2

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,23 +3,20 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
   const friends = [
-    // key는 문자열이어야 하고 유니크한 값이어야 한다.
-    { name: 'Friend #1', key: '1' },
-    { name: 'Friend #2', key: '2' },
-    { name: 'Friend #3', key: '3' },
-    { name: 'Friend #4', key: '4' },
-    { name: 'Friend #5', key: '5' },
-    { name: 'Friend #6', key: '6' },
-    { name: 'Friend #7', key: '7' },
-    { name: 'Friend #8', key: '8' },
-    { name: 'Friend #9', key: '9' }
+    { name: 'Friend #1' },
+    { name: 'Friend #2' },
+    { name: 'Friend #3' },
+    { name: 'Friend #4' },
+    { name: 'Friend #5' },
+    { name: 'Friend #6' },
+    { name: 'Friend #7' },
+    { name: 'Friend #8' },
+    { name: 'Friend #9' }
   ];
   return (
     <FlatList 
+      keyExtractor={friend => {friend.name}}
       data={friends} 
-      // renderItem={(element) => {
-      //   // element === {item: {name: 'Friend #1', index: 0}}
-      // }}
       renderItem={({item}) => {
         return <text>{item.name}</text>;
       }}
